8b352ee33476b981f0dc20b6cb1bf4486964e165
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2022-03-20 12:57:29 -0300

    feat(): others types(scalar types)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import { ApolloServer, gql } from 'apollo-server';
 
 const server = new ApolloServer({
-  typeDefs: gql`
-    type Query {
-      hello: String
-      world: String
-    }
-  `,
-  resolvers: {
-    Query: {
-      hello: () => 'Hello world!',
-      world: () => 'Hello world!',
-    },
-  },
+	typeDefs: gql`
+		type Query {
+			id: ID
+			name: String
+			age: Int
+			average: Float
+			dev: Boolean!
+			techs: [String!]!
+		}
+	`,
+	resolvers: {
+		Query: {
+			id: () => '1',
+			name: () => 'ricardo',
+			age: () => 24,
+			average: () => 7.5,
+			dev: () => true,
+			techs: () => ['React', 'Node', 'GraphQL'],
+		},
+	},
 });
 
 server.listen(4003).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+	console.log(`🚀  Server ready at ${url}`);
 });
