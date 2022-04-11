1f4093537122720185c0d98a41cb92250121cd80
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2022-04-11 19:17:55 -0300

    refactor: remove test resolvers

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -1,22 +1,6 @@
 const post = async (_, { id }, { getPosts }) => {
 	const post = await getPosts('/' + id);
 
-	if (Math.random() > 0.5) {
-		return {
-			statusCode: 500,
-			message: 'Post timeout',
-			timeout: 123,
-		};
-	}
-
-	if (typeof post.id === 'undefined') {
-		return {
-			statusCode: 404,
-			message: 'Post not found',
-			postId: id,
-		};
-	}
-
 	return post;
 };
 
@@ -27,41 +11,5 @@ const posts = (_, { input }, { getPosts }) => {
 };
 
 export const postResolvers = {
-	Query: {
-		post,
-		posts,
-	},
-	Post: {
-		unixTimestamp: ({ createdAt }) => {
-			const date = new Date(createdAt).getTime() / 1000;
-			return Math.floor(date);
-		},
-	},
-
-	PostResult: {
-		__resolveType: (obj) => {
-			if (typeof obj.postId !== 'undefined') {
-				return 'PostNotFoundError';
-			}
-
-			if (typeof obj.timeout !== 'undefined') {
-				return 'PostTimeoutError';
-			}
-
-			if (typeof obj.id !== 'undefined') return 'Post';
-			return null;
-		},
-	},
-
-	PostError: {
-		__resolveType: (obj) => {
-			if (typeof obj.postId !== 'undefined') {
-				return 'PostNotFoundError';
-			}
-
-			if (typeof obj.timeout !== 'undefined') {
-				return 'PostTimeoutError';
-			}
-		},
-	},
+	Query: { post, posts },
 };
