2728aec4f05dae93d2acd5409849ea0d0151e357
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2022-04-14 21:55:10 -0300

    feat: add post dataloaders

--- src/graphql/context.js
+++ src/graphql/context.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 import { getUsers } from './user/utils';
 import { getPosts } from './post/utils';
 import { makeUserDataLoader } from './user/dataloaders';
+import { makePostDataLoader } from './post/dataloaders';
 
 export const context = () => {
 	return {
 		userDataLoader: makeUserDataLoader(getUsers(axios)),
+		postDataLoader: makePostDataLoader(getPosts(axios)),
 		getUsers: getUsers(axios),
 		getPosts: getPosts(axios),
 	};
--- src/graphql/user/resolvers.js
+++ src/graphql/user/resolvers.js
@@ -6,9 +6,16 @@ const users = async (_, __, { getUsers }) => {
 	return getUsers();
 };
 
+const posts = async ({ id }, _, { postDataLoader }) => {
+	return postDataLoader.load(id);
+};
+
 export const userResolvers = {
 	Query: {
 		user,
 		users,
+	},
+	User: {
+		posts,
 	},
 };
